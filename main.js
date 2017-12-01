dda92c8ec2dbadd7047606b55f581fb7c40ebf8f
nona-sarokina <forgakk@gmail.com>
2017-12-01 09:55:57 +0300

    Update main.js
    revert

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ $(function () {
     setInterval(f, 1000);
     
     function f() {
-        var tLeft = Math.floor(Math.random()*100000),
-            tTop  = Math.floor(Math.random()*10000);
+        var tLeft = Math.floor(Math.random()*1000),
+            tTop  = Math.floor(Math.random()*1000);
 
         var style = "position: absolute; left:" +  tLeft + "; top: " + tTop + "";
         document.getElementById("container").innerHTML += "<div style='" + style + "'><img src='images/cat.ico'/> <span class='mew'>мяф </span></div>";
