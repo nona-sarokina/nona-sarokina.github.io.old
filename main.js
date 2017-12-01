d9607180b58ab2da7c7541da98ae821ab25e0085
nona-sarokina <forgakk@gmail.com>
2017-12-01 09:53:37 +0300

    Update main.js
    left and right calculation changes

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 $(function () {
-
     setInterval(f, 1000);
+    
     function f() {
-        var tLeft = Math.floor(Math.random()*500),
-            tTop  = Math.floor(Math.random()*500);
+        var tLeft = Math.floor(Math.random()*10000),
+            tTop  = Math.floor(Math.random()*10000);
 
         var style = "position: absolute; left:" +  tLeft + "; top: " + tTop + "";
-
-
         document.getElementById("container").innerHTML += "<div style='" + style + "'><img src='images/cat.ico'/> <span class='mew'>мяф </span></div>";
     }
-
 });
 
