fffc0a7dc970f7604263173bd6721c59ec4b800d
Nona <forgakk@gmail.com>
2017-12-27 02:55:24 +0300

    horizontal kicker chart adding

---
+++ js/chart.js
@@ -0,0 +1,74 @@
+(function () {
+    var module = angular.module("chart", ["chart.js"]);
+    module.factory('players', function ($http) {
+        return {
+            all: function (callback) {
+                $http.get('data/data.json').then(callback);
+            }
+        };
+    });
+
+
+    module.controller("BarCtrl", function ($scope, $http) {
+        $http.get('data/data.json').then(function (response) {
+            $scope.players = response.data;
+            $scope.selectedPlayers = [];
+        });
+
+
+        $scope.updateChart = function (player) {
+            var selectedPlayers = $scope.selectedPlayers;
+            if (selectedPlayers.indexOf(player) != -1) selectedPlayers.splice(selectedPlayers.indexOf(player), 1)
+            else selectedPlayers.push(player)
+            $scope.labels = [];
+            $scope.series = [];
+            $scope.data = [];
+
+            var games = prepareGameData();
+
+            addLabels(games);
+            addData(games);
+
+            function prepareGameData() {
+                var games = [];
+                angular.forEach(selectedPlayers, function (value, key) {
+                    var playerName = value.name;
+                    $scope.series.push(playerName);
+
+                    angular.forEach(value.games, function (value, key) {
+                        var game = value;
+                        game.number = key;
+                        game.playerName = playerName;
+                        games.push(game);
+                    });
+                    games.sort(function (a, b) {
+                        return a.number - b.number;
+                    });
+                });
+                return games;
+            }
+
+            function addLabels(games) {
+                angular.forEach(games, function (value, key) {
+                    if ($scope.labels.indexOf(value.number) == -1) {
+                        $scope.labels.push(value.number)
+                    }
+                });
+            }
+
+            function addData() {
+                angular.forEach($scope.series, function (player, key) {
+                    var currentPlayerData = [];
+                    angular.forEach(games, function (value, key) {
+                        if (value.playerName === player) {
+                            currentPlayerData.push(value.rank);
+                        }
+                    });
+                    $scope.data.push(currentPlayerData);
+                });
+            }
+        }
+    });
+
+})();
+
